fd1631ce45f3c495cdaa2cccce35f1f23878ec9f
David Zurita <108833714+DavidZuSan@users.noreply.github.com>
2023-10-05 00:45:34 +0200

    Update

--- netlify-functions/subscribe.cjs
+++ netlify-functions/subscribe.cjs
@@ -1,18 +1,26 @@
 const apikey = process.env.MAILCHIMP_API_KEY;
-
 const fetch = require("node-fetch");
 
 exports.handler = async (event) => {
+  // Registro del evento recibido para depuración
+  console.log("Received event:", event);
+
   // Asegurarte de que el método es POST
   if (event.httpMethod !== "POST") {
     return { statusCode: 405, body: "Method Not Allowed" };
   }
 
   // Parsear el cuerpo de la solicitud
-  const { email } = JSON.parse(event.body);
+  const { email_address } = JSON.parse(event.body);
 
-  if (!email) {
-    return { statusCode: 400, body: "Email is required" };
+  if (!email_address) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        error: "Email is required",
+        receivedData: JSON.parse(event.body),
+      }),
+    };
   }
 
   // URL de la API de Mailchimp
@@ -20,7 +28,7 @@ exports.handler = async (event) => {
 
   // Datos a enviar a Mailchimp
   const data = {
-    email_address: email,
+    email_address: email_address,
     status: "subscribed",
   };
 
@@ -35,17 +43,21 @@ exports.handler = async (event) => {
       body: JSON.stringify(data),
     });
 
+    const responseText = await response.text();
+    console.log("Respuesta de Mailchimp:", responseText);
+
     // Verificar si la respuesta es exitosa
     if (!response.ok) {
-      const errorResponse = await response.json();
-      console.error("Mailchimp error", errorResponse);
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
 
     // Devolver una respuesta exitosa
-    return { statusCode: 200, body: "Subscription successful" };
+    return {
+      statusCode: 200,
+      body: "¡Suscripción exitosa! Gracias por registrarte.",
+    };
   } catch (error) {
-    // Manejar y devolver errores
+    // Registro del error para depuración
     console.error("Subscription error", error);
     return { statusCode: 500, body: error.toString() };
   }
