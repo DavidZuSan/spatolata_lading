1a9834f39032a1da21409483c750fbf7326305c6
David Zurita <108833714+DavidZuSan@users.noreply.github.com>
2023-10-04 12:27:04 +0200

    End

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,52 @@
+import { useState, useEffect } from "react";
 import Footer from "./components/Footer/Footer";
 import HeroSection from "./components/HeroSection/HeroSection";
 import NavBar from "./components/NavBar/NavBar";
 import TopFlavours from "./components/TopFlavours/TopFlavours";
+import ArrowUpIcon from "./assets/icons/ArrowUpIcon";
+import "./index.css";
 
 const App = () => {
+  const [showButton, setSchowButton] = useState(false);
+
+  useEffect(() => {
+    const checkScroll = () => {
+      if (window.scrollY >= window.innerHeight / 2) {
+        setSchowButton(true);
+      } else {
+        setSchowButton(false);
+      }
+    };
+
+    window.addEventListener("scroll", checkScroll);
+
+    return () => {
+      window.removeEventListener("scroll", checkScroll);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <div>
       <NavBar />
       <HeroSection />
       <TopFlavours />
       <Footer />
+      {showButton && (
+        <button
+          onClick={scrollToTop}
+          className="scrollToTopButton"
+          aria-label="Scroll to top"
+        >
+          <ArrowUpIcon />
+        </button>
+      )}
     </div>
   );
 };
